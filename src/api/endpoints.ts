f572e1c3762398f3df4901947eef9a544368ac87
kole <Kfnunley@gmail.com>
2021-07-09 08:23:29 -0400

    client side prediction request/endpoint

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,6 +1,9 @@
 import request from './request';
-import { LoginAsGuestResponse, DBToken, TwitchChannelPageData } from '../common/types';
+import {
+  LoginAsGuestResponse, DBToken, TwitchChannelPageData, Prediction, PredictionBase,
+} from '../common/types';
 
 export const loginAsGuest = () => request<LoginAsGuestResponse>('POST', '/api/auth/loginAsGuest');
 export const getDBToken = () => request<DBToken>('GET', '/api/auth/dbToken');
 export const getTwitchChannelPageData = (channelName: string) => request<TwitchChannelPageData>('GET', `/api/twitch/${channelName}`);
+export const predict = (data: PredictionBase) => request<Prediction>('POST', '/api/predict', data);
--- src/api/request.ts
+++ src/api/request.ts
@@ -1,5 +1,5 @@
-export default async <T>(method: string, path: string) : Promise<T> => {
-  const res = await fetch(path, { method });
+export default async <T>(method: string, path: string, body?: any) : Promise<T> => {
+  const res = await fetch(path, { method, body: body ? JSON.stringify(body) : undefined });
   if (res.status === 200) {
     return (await res.json()) as T;
   }
